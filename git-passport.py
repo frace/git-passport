1acce2336e1bac9fe3bf75c1ee5f860db8c14cf0
frace <frace@users.noreply.github.com>
2015-01-09 18:16:57 +0100

    Changed script comment

--- git-passport.py
+++ git-passport.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-""" A Git pre-commit hook written in Python to manage multiple Git
-    user identities.
+""" git-passport is a Git pre-commit hook written in Python to manage
+    multiple Git user identities.
 """
 
 
